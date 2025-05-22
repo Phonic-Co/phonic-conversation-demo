5e9e0b5a5842c040d6506b189e27455ff36269e6
Qiong Zhou Huang <qiong@phonic.co>
2025-05-22 23:51:29 +0000

    UNIFY MAVEN!
    Signed-off-by: Qiong Zhou Huang <qiong@phonic.co>

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,29 @@ const app = new Hono();
 
 const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app });
 
-let inboundFromPhoneNumber: string | null = null;
+const welcome_messages = {
+  "rockstar": "Hi, this is Emma. Thanks for calling Rockstar. How can I help you today?",
+  "cvent": "Hi, this is Anna. Thanks for calling Sea vent. How can I help you today?",
+  "crmspot": "Hi, this is Emma. Thanks for calling CRM Spot. How can I help you today?",
+  "rockefeller": "Hi, this is Emma. Thanks for calling the Rockefeller Center. How can I help you today?",
+}
+
+const recieving_number_to_customer_map = {
+  "+15168537827": "rockstar",
+  "+15853674664": "rockefeller",
+  "+16822228368": "cvent",
+  "+15413293276": "crmspot",
+  "+12134637625": "rockefeller",
+}
 
 app.post("/inbound", async (c) => {
   // Print the POST request body
   const body = await c.req.parseBody();
   console.log("FROM PHONE NUMBER:", body.From);
-  inboundFromPhoneNumber = body.From as string;
+  const inboundFromPhoneNumber = body.From as string;
+  const inboundRecievingPhoneNumber = body.To as string;
+
+  const customer = recieving_number_to_customer_map[inboundRecievingPhoneNumber as keyof typeof recieving_number_to_customer_map] || "cvent";
 
   const url = new URL(c.req.url);
   const response = new VoiceResponse();
@@ -28,6 +44,13 @@ app.post("/inbound", async (c) => {
   stream.parameter({
     name: "phoneNumber",
     value: inboundFromPhoneNumber,
+  });
+
+  console.log("CUSTOMER:", customer);
+
+  stream.parameter({
+    name: "customer",
+    value: customer,
   });
 
   return c.text(response.toString(), 200, { "Content-Type": "text/xml" });
@@ -59,15 +82,21 @@ app.get(
             const phoneNumber = messageObj.start.customParameters.phoneNumber;
             console.log("[INBOUND-WS] PHONE NUMBER:", phoneNumber);
 
+            // @ts-ignore
+            const customer = messageObj.start.customParameters.customer;
+
             phonic = setupPhonic(ws, c, {
               project: "maven",
               input_format: "mulaw_8000",
-              welcome_message: "Hi, this is Emma. Thanks for calling Rockstar Mobile. How can I help you today?",
+              welcome_message: welcome_messages[customer as keyof typeof welcome_messages] || "Welcome to Phonic! How can I help you today?",
               voice_id: "greta",
               system_prompt: "",
               output_format: "mulaw_8000",
               vad_threshold: 0.5,
-              phone_number: phoneNumber,
+              experimental_params: {
+                phone_number: phoneNumber,
+                customer: customer,
+              },
             });
 
             phonic.setExternalId(messageObj.start.callSid);
